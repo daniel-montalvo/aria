5382002f8764a552babb76e7a43a83ee9fbdab79
Daniel <dmontalvo@w3.org>
2026-05-29 17:28:50 +0200

    tries wrapping build process on a function

--- common/script/pr-preview.sh
+++ common/script/pr-preview.sh
@@ -23,27 +23,37 @@ mkdir -p public/pdf-aam
 mkdir -p public/css-aam
 
 # build all specs
-respec -s index.html -o public/index.html --localhost
-echo "Built ARIA"
-respec -s accname/index.html -o public/accname/index.html --localhost
-echo "Built AccName"
-respec -s core-aam/index.html -o public/core-aam/index.html --localhost
-echo "Built Core AAM"
-respec -s dpub-aam/index.html -o public/dpub-aam/index.html --localhost
-echo "Built DPub AAM"
-respec -s dpub-aria/index.html -o public/dpub-aria/index.html --localhost
-echo "Built DPUB ARIA"
-respec -s graphics-aam/index.html -o public/graphics-aam/index.html --localhost
-echo "Built graphics AAM"
-respec -s graphics-aria/index.html -o public/graphics-aria/index.html --localhost
-echo "Built graphics ARIA"
-respec -s html-aam/index.html -o public/html-aam/index.html --localhost
-echo "Built HTML AAM"
-respec -s svg-aam/index.html -o public/svg-aam/index.html --localhost
-echo "Built SVG AAM"
-respec -s mathml-aam/index.html -o public/mathml-aam/index.html --localhost
-echo "Built mathml-aam"
-respec -s pdf-aam/index.html -o public/pdf-aam/index.html --localhost
-echo "Built PDF AAM"
-respec -s css-aam/index.html -o public/css-aam/index.html --localhost
-echo "Built CSS  AAM"
+LOGFILE="public/respec-errors.log"
+: > "$LOGFILE"
+failed=0
+
+build_spec() {
+	local src="$1"
+	local out="$2"
+	local name="$3"
+
+	if respec -s "$src" -o "$out" --localhost --verbose >> "$LOGFILE" 2>&1; then
+		echo "Built $name"
+	else
+		echo "Respec failed: $name ($src -> $out). See $LOGFILE" >&2
+		failed=1
+	fi
+}
+
+build_spec "index.html" "public/index.html" "ARIA"
+build_spec "accname/index.html" "public/accname/index.html" "AccName"
+build_spec "core-aam/index.html" "public/core-aam/index.html" "Core AAM"
+build_spec "dpub-aam/index.html" "public/dpub-aam/index.html" "DPub AAM"
+build_spec "dpub-aria/index.html" "public/dpub-aria/index.html" "DPUB ARIA"
+build_spec "graphics-aam/index.html" "public/graphics-aam/index.html" "graphics AAM"
+build_spec "graphics-aria/index.html" "public/graphics-aria/index.html" "graphics ARIA"
+build_spec "html-aam/index.html" "public/html-aam/index.html" "HTML AAM"
+build_spec "svg-aam/index.html" "public/svg-aam/index.html" "SVG AAM"
+build_spec "mathml-aam/index.html" "public/mathml-aam/index.html" "mathml-aam"
+build_spec "pdf-aam/index.html" "public/pdf-aam/index.html" "PDF AAM"
+build_spec "css-aam/index.html" "public/css-aam/index.html" "CSS  AAM"
+
+if [[ "$failed" -ne 0 ]]; then
+	echo "One or more specs failed to build. See $LOGFILE" >&2
+	exit 1
+fi
